a6931386f7a38a71dfb017b959b4394c16ffc593
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-01-26 21:26:56 -0300

    refactor: move ducplicated code to sut function

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('jsonwebtoken', () => ({
   }
 }))
 
+const makeStu = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
 describe('Jwt Adapter', () => {
   it('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeStu()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('Should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeStu()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
 
   it('Should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeStu()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error()
     })
