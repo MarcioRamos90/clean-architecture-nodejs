380b0b4f79c7b0e4d57a8119490abe227a03908a
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-02-18 00:03:21 -0300

    refactor: rename SignUp with Login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
 
-describe('SinUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,15 +15,18 @@ describe('SinUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should reuturns an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Marcio',
-        email: 'marcio.ramos@mail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+
+  describe('POST /signup', () => {
+    test('Should returns 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Marcio',
+          email: 'marcio.ramos@mail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -3,5 +3,6 @@ import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
